f33db0d6b214fe7d86a1a812b8b4c9375c87e337
Passam <119688260+Passam02@users.noreply.github.com>
2023-07-23 16:50:28 +0200

    Add Navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import App from 'next/app'
+import NavBar from '../components/NavBar'
 import '../styles/styles.css'
  
 export default function MyApp({ Component, pageProps, example }) {
   return (
     <>
+      <NavBar/>
       <Component {...pageProps} />
     </>
   )
