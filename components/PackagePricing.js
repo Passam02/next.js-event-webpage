91bf4454cb4ce1c0d0e2c9de9c69ccf55f20348e
Passam <119688260+Passam02@users.noreply.github.com>
2023-07-26 12:59:36 +0200

    Add PackagePricing component

---
+++ components/PackagePricing.js
@@ -0,0 +1,91 @@
+import { Paper, Typography, Divider, Box, Grid } from "@mui/material"
+import Image from "next/image"
+
+//<Divider sx={{marginTop: "24px"}} orientation="vertical" variant="middle" flexItem/>
+
+const PackagePricing = () => {
+    return(
+        <Paper sx={{
+            display: "flex",
+            flexDirection: "row",
+            mt: 20,
+            maxWidth: 900,
+            marginLeft: "auto",
+            marginRight: "auto",
+            boxShadow: "0px 0px 25px -7px rgba(66, 68, 90, 1)"
+        }}>
+            <Grid container spacing={3}>
+                <Grid item xs={4} sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingTop: "0px !important",
+                    marginTop: "24px",
+                    alignItems: "center"
+                }}>
+                    <Box sx={{
+                        width: "80%",
+                        alignItems: "center",
+                        textAlign: "center"
+                    }}>
+                        <Typography variant="h3" mt={1}>BASIC</Typography>
+                        <Typography variant="h4" mb={2}>30 $</Typography>
+                        <Typography>Access to convent</Typography>
+                        <Divider/>
+                        <Typography>Attendee Badge</Typography>
+                        <Divider/>
+                        <Typography>Soft Drinks</Typography>
+                        <Divider/>
+                        <Typography mb={2}>Free Breakfast's</Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={4} sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingTop: "0px !important",
+                    marginTop: "24px",
+                    borderLeft: "grey 1px solid",
+                    borderRight: "grey 1px solid",
+                    alignItems: "center"
+                }}>
+                    <Box sx={{
+                        width: "80%",
+                        alignItems: "center",
+                        textAlign: "center"
+                    }}>
+                        <Typography variant="h3" mt={1}>SPONSOR</Typography>
+                        <Typography variant="h4" mb={2}>50 $</Typography>
+                        <Typography>+ All Basic Perks</Typography>
+                        <Divider/>
+                        <Typography>+ Custom Attendee Badge</Typography>
+                        <Divider/>
+                        <Typography>+ Special Gift For Sponsors</Typography>
+                        <Divider/>
+                        <Typography mb={2}>+ Free Parking Spot</Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={4} sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingTop: "0px !important",
+                    marginTop: "24px",
+                    alignItems: "center"
+                }}>
+                    <Box sx={{
+                        width: "80%",
+                        alignItems: "center",
+                        textAlign: "center"
+                    }}>
+                        <Typography variant="h3" mt={1}>FOUNDER</Typography>
+                        <Typography variant="h4" mb={2}>75 $</Typography>
+                        <Typography>+ Basic And Sponsor Perks </Typography>
+                        <Divider/>
+                        <Typography>+ Limited Alcohol Drinks</Typography>
+                        <Divider/>
+                        <Typography mb={2}>+ Limited T-Shirt</Typography>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Paper>
+    )
+}
+export default PackagePricing
---
+++ pages/pricing.js
@@ -0,0 +1,9 @@
+import { Paper, Box } from "@mui/material"
+import PackagePricing from "../components/PackagePricing"
+
+const pricing = () => {
+    return(
+            <PackagePricing/>
+    )
+}
+export default pricing
