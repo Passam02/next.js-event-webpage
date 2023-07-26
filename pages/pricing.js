743e326c1bb18c47fb45c2e653524230339a3c27
Passam <119688260+Passam02@users.noreply.github.com>
2023-07-26 13:16:25 +0200

    Improve PackagePricing Component

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,9 +1,12 @@
-import { Paper, Box } from "@mui/material"
 import PackagePricing from "../components/PackagePricing"
+import HotelPricing from "../components/HotelPricing"
 
 const pricing = () => {
     return(
+        <>
             <PackagePricing/>
+            <HotelPricing/>
+        </>
     )
 }
 export default pricing
